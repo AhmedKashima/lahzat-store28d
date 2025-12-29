a8a176e2e549cba8dc80eea46cc50a74c542ae78
kashimaa <kashimaahmed@gmail.com>
2025-12-29 23:43:23 +0300

    movingwsgitoStore

--- store/wsgi.py
+++ store/wsgi.py
@@ -17,7 +17,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 # application = get_wsgi_application()
 
+import os
+from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
+
+application = get_wsgi_application()
 import os
 from django.core.wsgi import get_wsgi_application
 
