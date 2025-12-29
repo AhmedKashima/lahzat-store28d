03e199163f6b095cd95027ed2c491c4bbfc25095
kashimaa <kashimaahmed@gmail.com>
2025-12-29 23:31:56 +0300

    updateforrender

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -7,12 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.store.settings')
+
+# application = get_wsgi_application()
+
+
+import os
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.store.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
 
 application = get_wsgi_application()
